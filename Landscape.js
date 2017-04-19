cb404daaaedea8107b72417cb22b50770869f61e
Be <bebirchall@gmail.com>
2017-04-19 15:18:44 -0400

    Add view styles to toggle between: light, dark, birds eye view

--- Landscape.js
+++ Landscape.js
@@ -15,6 +15,7 @@
     MAROON:       0x512544,
     TEAL:         0x017a8b,
 
+    BLACK:        0x000000,
     GRAY:         0x808080,
     LIGHT_GRAY:   0xd3d3d3,
     WHITE:        0xffffff,
@@ -25,6 +26,8 @@
     PT_4: {lat: 40.7329915, lng: -73.9879134},  // 13th and 3rd
     PT_5: {lat: 40.720579,  lng: -73.995273},   // cafe integral
 
+    PT_6: {lat: 40.674977, lng: -73.976649},    // PSFC
+
     // RC coords used as map origin
     ORIGIN_LNG:   -74.001002,
     ORIGIN_LAT:   40.720878,
@@ -32,17 +35,28 @@
     MAP_WIDTH:    1000000,
     MAP_HEIGHT:   1000000,
 
+    BEV_HEIGHT: 150, // how high up for bird's eye view
+
     renderer: null,
     scene: null,
     camera: null,
+    grid: null,
 
     dir: null,
     lat: null,
     lng: null,
     proj: null,
 
+    viewType: null, // integer, representing view type in map below
+    viewTypeMap: { // map of view types to description
+      0: 'light theme',
+      1: 'dark theme',
+      2: 'bird\'s eye view, dark theme'
+    },
+
     init: function() {
       this.$update = $('#update');
+      this.$viewToggle = $('#view-toggle');
       this.$container = $('#container');
 
       this.width = window.innerWidth;
@@ -52,6 +66,7 @@
       this.scene = new THREE.Scene();
       this.camera = new THREE.PerspectiveCamera(75, this.aspect, 0.1, 10000);
       this.renderer = new THREE.WebGLRenderer({antialias: true});
+      this.renderer.setSize(this.width, this.height);
 
       this.proj = new Proj(
           this.MAP_WIDTH, this.MAP_HEIGHT, this.ORIGIN_LAT, this.ORIGIN_LNG
@@ -60,13 +75,9 @@
       this.lat = this.ORIGIN_LAT;
       this.lng = this.ORIGIN_LNG;
 
-      this.scene.fog = new THREE.Fog(this.WHITE, 0.1, 200);
-      this.renderer.setSize(this.width, this.height);
-      this.renderer.setClearColor(this.LIGHT_GRAY, 1);
-
       this.addMarkers();
-      this.addGrid();
       this.light();
+      this.viewTypeSet(0);
 
       this.$container.append(this.renderer.domElement);
       this.render();
@@ -74,6 +85,7 @@
 
     wire: function() {
       this.$update.on('click', this.updateH.bind(this));
+      this.$viewToggle.on('click', this.viewToggleH.bind(this));
       window.addEventListener('deviceorientation', this.orientationH.bind(this), true);
     },
 
@@ -86,10 +98,15 @@
     },
 
     addGrid: function() {
-      var gridHelper = new THREE.GridHelper(300, 20);
-      gridHelper.position.y = -8;
+      this.grid = new THREE.GridHelper(300, 20);
+      this.grid.position.y = -8;
 
-      this.scene.add(gridHelper);
+      this.scene.add(this.grid);
+    },
+
+    removeGrid: function() {
+      this.scene.remove(this.grid);
+      this.grid = null;
     },
 
     createMarker: function(color) {
@@ -119,11 +136,13 @@
       this.addMarker(this.ORANGE, this.PT_3);
       this.addMarker(this.MAROON, this.PT_4);
       this.addMarker(this.TEAL, this.PT_5);
+
+      this.addMarker(this.TEAL, this.PT_6);
     },
 
     cameraPos: function(lat, lng, height) {
       if (typeof height === 'undefined') {
-        var height = 0;
+        var height = (this.viewType == 2) ? this.BEV_HEIGHT : 0;
       }
 
       var coords = this.proj.coordsMap(lat, lng);
@@ -154,6 +173,55 @@
       this.dir = (typeof compass === 'number') ? compass : - alpha;
     },
 
+    viewTypeSet: function(n) {
+      console.log('update to type: ', this.viewTypeMap[n]);
+
+      switch(n) {
+        case 0: // light theme
+          this.viewType = 0;
+          this.renderer.setClearColor(this.LIGHT_GRAY, 1);
+          if (this.grid === null) this.addGrid();
+
+          this.camera.position.y = 0;
+          //this.scene.fog = new THREE.Fog(this.White, 0,1, 200);
+          break;
+
+        case 1: // dark theme
+          this.viewType = 1;
+          this.renderer.setClearColor(this.BLACK, 1);
+          if (this.grid !== null) this.removeGrid();
+
+          this.camera.position.y = 0;
+          break;
+
+        case 2: // bird's eye view
+          this.viewType = 2;
+          this.renderer.setClearColor(this.BLACK, 1);
+          if (this.grid !== null) this.removeGrid();
+
+          this.bev(this.BEV_HEIGHT);
+          break;
+
+        default:
+          throw new Error('view type not supported');
+      }
+    },
+
+    viewToggleH: function() {
+      var map = { // from type: to type (see this.viewTypeMap for descriptions)
+        0: 1,
+        1: 2,
+        2: 0
+      };
+
+      if (typeof map[this.viewType] === 'undefined') {
+        throw new Error('type not supported by toggle handler');
+      }
+      else {
+        this.viewTypeSet(map[this.viewType]);
+      }
+    },
+
     updateH: function() {
       window.navigator.geolocation.getCurrentPosition(function(pos) {
         var coords = pos.coords;
@@ -164,18 +232,17 @@
         console.log('lat: ', this.lat);
         console.log('lng: ', this.lng);
 
-        this.cameraPos(this.lat, this.lng, 0);
+        this.cameraPos(this.lat, this.lng);
       }.bind(this));
     },
 
-    // bird's eye view
     bev: function(height) {
-      var origin = new THREE.Vector3(0, 0, 0);
+      var x = this.camera.position.x;
+      var z = this.camera.position.z;
+      var below = new THREE.Vector3(x, 0, z);
 
-      this.camera.position.x = 0;
       this.camera.position.y = height;
-      this.camera.position.z = 0;
-      this.camera.lookAt(origin);
+      this.camera.lookAt(below);
     },
 
     render: function() {
